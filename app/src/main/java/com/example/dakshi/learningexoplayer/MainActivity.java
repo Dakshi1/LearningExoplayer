1464163dda550d9dadc92ebd089de692d2437315
Dakshi Suri <dakshi884@gmail.com>
2018-02-21 19:08:22 +0530

    audio and video together

--- app/src/main/java/com/example/dakshi/learningexoplayer/MainActivity.java
+++ app/src/main/java/com/example/dakshi/learningexoplayer/MainActivity.java
@@ -12,6 +12,8 @@ import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
@@ -53,15 +55,34 @@ public class MainActivity extends AppCompatActivity {
         player.setPlayWhenReady(playWhenReady);
         player.seekTo(currentWindow, playbackPosition);
 
-        Uri uri = Uri.parse(getString(R.string.media_url1_mp3));
+        Uri uri = Uri.parse(getString(R.string.media_url3_mp3));
         MediaSource mediaSource = buildMediaSource(uri);
         player.prepare(mediaSource, true, false);
     }
 
     private MediaSource buildMediaSource(Uri uri) {
-        return new ExtractorMediaSource.Factory(
+        /*return new ExtractorMediaSource.Factory(
                 new DefaultHttpDataSourceFactory("exoplayer-codelab")).
-                createMediaSource(uri);
+                createMediaSource(uri);*/
+
+        // these are reused for both media sources we create below
+        DefaultExtractorsFactory extractorsFactory =
+                new DefaultExtractorsFactory();
+        DefaultHttpDataSourceFactory dataSourceFactory =
+                new DefaultHttpDataSourceFactory( "user-agent");
+
+        ExtractorMediaSource videoSource =
+                new ExtractorMediaSource.Factory(
+                        new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+                        createMediaSource(uri);
+
+        Uri audioUri = Uri.parse(getString(R.string.media_url_mp3));
+        ExtractorMediaSource audioSource =
+                new ExtractorMediaSource.Factory(
+                        new DefaultHttpDataSourceFactory("exoplayer-codelab")).
+                        createMediaSource(audioUri);
+
+        return new ConcatenatingMediaSource(audioSource, videoSource);
     }
 
     private void releasePlayer() {
